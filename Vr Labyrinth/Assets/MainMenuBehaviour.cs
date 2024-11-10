c38cdc0ae7bd72d2137843a86a6a2bd70a985e77
sebash3151 <Juan.marind@upb.edu.co>
2024-11-09 19:13:09 -0500

    Changes to map and enemy behaviour

---
+++ Vr Labyrinth/Assets/MainMenuBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("empexzo el jeugo ");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
--- Vr Labyrinth/Assets/Scripts/NavMeshController.cs
+++ Vr Labyrinth/Assets/Scripts/NavMeshController.cs
@@ -2,22 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
 
 public class NavMeshController : MonoBehaviour
 {
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    [SerializeField] private bool isPatroling = true;
 
-    [SerializeField] Transform objetivo;
-    private NavMeshAgent agente;
-    // Start is called before the first frame update
+    [SerializeField] Transform playerTrans;
+    [SerializeField] private bool isHunting = false;
+
     void Start()
     {
-        agente = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
     }
 
-    // Update is called once per frame
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
     void Update()
     {
-        agente.destination = objetivo.position;
-
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (isPatroling)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }else if (isHunting)
+        {
+            agent.destination = playerTrans.position;
+        }
+        
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPatroling = false;
+            isHunting = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPatroling = true;
+            isHunting = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Head Follower")
+        {
+            //Debug.Log("daougnvaosfuiygbasfg");
+            //isPatroling = false;
+            //isHunting = false;
+            ReiniciarEscena();
+        }
+    }
+
+    void ReiniciarEscena()
+    {
+        //Debug.Log("reicino");
+
+        // Obtiene el nombre o índice de la escena actual
+        string escenaActual = SceneManager.GetActiveScene().name;
+
+        // Carga la escena actual nuevamente
+        SceneManager.LoadScene(escenaActual);
+    }
+
+
 }
